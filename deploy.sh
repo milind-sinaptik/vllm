6eb3027e76710c40f2c5d6f565243fcdbfa6c301
Milind Lalwani <milindlalwani@Milinds-MacBook-Air.local>
2023-12-11 15:59:04 +0100

    testing cd

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ ssh -o StrictHostKeyChecking=no $REMOTE_USER@$REMOTE_HOST <<EOF
   pip install -e .
   echo -e "$HUGGING_FACE_TOKEN\nn" | huggingface-cli login
   timestamp=$(date "+%Y%m%d%H%M%S")
-  python -m vllm.entrypoints.api_server --model="$LLM_NAME" --trust-remote-code > "std_$timestamp.out" &
+  python -m vllm.entrypoints.api_server --model="$LLM_NAME" --trust-remote-code > "std_$timestamp.out" 2>&1 &
 EOF
 
 echo "Deployment to $DEPLOY_ENVIRONMENT completed successfully."
